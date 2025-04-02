543b963e2184c0772890c690cdda15e5bb8c7b4d
Yujin <yujinyll9148@gmail.com>
2025-04-02 12:48:39 +0900

    feat: custom toast message ui

--- apps/fe/src/providers/toastProvider.tsx
+++ apps/fe/src/providers/toastProvider.tsx
@@ -4,11 +4,31 @@ import { ReactNode } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+const contextClass = {
+  success: 'bg-blue-100 text-blue-500 border-blue-200',
+  error: 'bg-red-100 text-red-500 border-red-200',
+  info: 'bg-purple-100 text-purple-500 border-purple-200',
+  warning: 'bg-yellow-50 text-yellow-500 border-yellow-200',
+  default: 'bg-white text-gray-800 border-gray-200',
+};
+
 export default function ToastProvider({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
-      <ToastContainer />
+      <ToastContainer
+        position="bottom-center"
+        hideProgressBar
+        pauseOnHover
+        closeOnClick
+        closeButton={false}
+        icon={() => null}
+        toastClassName={(context) =>
+          contextClass[context?.type || 'default'] +
+          ' ' +
+          'text-body-sm px-4 py-2 rounded-full border shadow-xs cursor-pointer'
+        }
+      />
     </>
   );
 }
