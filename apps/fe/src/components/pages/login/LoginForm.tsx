7935934702acdba275731d2f705abe885b3762d1
Yujin <yujinyll9148@gmail.com>
2025-04-03 10:49:19 +0900

    refactor: use axios and custom hook to login
    - added error handling
    - added new constant values

--- apps/fe/src/components/pages/login/LoginForm.tsx
+++ apps/fe/src/components/pages/login/LoginForm.tsx
@@ -1,25 +1,22 @@
 'use client';
 
 import { useState } from 'react';
-import { useRouter } from 'next/navigation';
 import BaseButton from '@/components/common/buttons/BaseButton';
 import TextInput from '@/components/common/inputs/TextInput';
 import LinkButton from './LinkButton';
 import { toast } from 'react-toastify';
 import ErrorMessage from '@/components/common/labels/ErrorMessage';
+import { MESSAGES } from '@/constants/messages';
+import { useLogin } from '@/hooks/useLogin';
 
 export default function LoginForm() {
-  const router = useRouter();
-
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState<string | null>(null);
 
   const [password, setPassword] = useState('');
   const [passwordError, setPasswordError] = useState<string | null>(null);
 
-  const [loginError, setLoginError] = useState<string | null>(null);
-
-  const [isLoading, setIsLoading] = useState(false);
+  const { isLoading, setIsLoading, submit, loginError, setLoginError } = useLogin();
 
   const clearErrors = () => {
     setEmailError(null);
@@ -28,28 +25,22 @@ export default function LoginForm() {
   };
 
   const validation = () => {
-    clearErrors();
-
-    // 이메일을 입력하지 않았을 경우
-    if (email == '') {
-      setEmailError('이메일을 입력해 주세요.');
-      setIsLoading(false);
-      return;
+    if (email === '') {
+      setEmailError(MESSAGES.emailRequired);
+      return false;
     }
 
-    // 비밀번호를 입력하지 않았을 경우
-    if (password == '') {
-      setPasswordError('비밀번호를 입력해 주세요.');
-      setIsLoading(false);
-      return;
+    if (password === '') {
+      setPasswordError(MESSAGES.passwordRequired);
+      return false;
     }
 
-    clearErrors();
+    return true;
   };
 
   const handleShowingComingSoonToast = (event: React.MouseEvent) => {
     event.preventDefault();
-    toast('해당 기능은 추후 업데이트될 예정입니다.', {
+    toast(MESSAGES.featureComingSoon, {
       type: 'info',
     });
   };
@@ -58,45 +49,14 @@ export default function LoginForm() {
     e.preventDefault();
 
     setIsLoading(true);
-    validation();
+    clearErrors();
 
-    try {
-      const response = await fetch('http://localhost:4000/api/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        credentials: 'include',
-        body: JSON.stringify({
-          UserId: email,
-          Password: password,
-        }),
-      });
-
-      const data = await response.json();
-
-      // 이메일 혹은 비밀번호가 틀렸을 경우
-      if (!response.ok && response.status == 401) {
-        setLoginError('이메일 또는 비밀번호가 일치하지 않습니다.');
-        setIsLoading(false);
-        return;
-      }
-
-      localStorage.setItem('accessToken', data.accessToken);
-
-      toast('로그인에 성공했습니다.', {
-        type: 'success',
-      });
-
-      router.push('/preview');
-    } catch (error) {
-      toast('일시적인 문제가 발생했습니다.', {
-        type: 'error',
-      });
-      console.error(error);
-    } finally {
+    if (!validation()) {
       setIsLoading(false);
+      return;
     }
+
+    await submit(email, password);
   };
 
   return (
