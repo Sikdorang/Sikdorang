e42d6d4b97a46ac26e73a4127ee72551fbe4af4d
Yujin <yujinyll9148@gmail.com>
2025-04-03 10:20:57 +0900

    refactor: use error message component in text input

--- apps/fe/src/components/common/inputs/TextInput.tsx
+++ apps/fe/src/components/common/inputs/TextInput.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { InputHTMLAttributes } from 'react';
+import ErrorMessage from '../labels/ErrorMessage';
 
 interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
@@ -14,7 +14,7 @@ export default function TextInput({ label, errorMessage, ...props }: TextInputPr
     <div className="flex flex-col">
       {label && <label className="text-gray-800 text-label-sm-m mb-2">{label}</label>}
       <input className={`${baseClass}`} {...props} />
-      {errorMessage && <span className="mt-1 animate-shake text-red-500 text-body-xs">{errorMessage}</span>}
+      {errorMessage && <ErrorMessage className="mt-1">{errorMessage}</ErrorMessage>}
     </div>
   );
 }
