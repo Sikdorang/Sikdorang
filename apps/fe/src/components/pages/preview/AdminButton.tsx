980a5017567e7e5ba2631854ff905748fa9eb003
Yujin <yujinyll9148@gmail.com>
2025-04-11 02:12:36 +0900

    fix: set modal for changing user mode to admin mode

--- apps/fe/src/components/pages/preview/AdminButton.tsx
+++ apps/fe/src/components/pages/preview/AdminButton.tsx
@@ -7,13 +7,29 @@ import TextInput from '@/components/common/inputs/TextInput';
 import BaseButton from '@/components/common/buttons/BaseButton';
 import BaseModal from '@/components/common/modals/BaseModal';
 import { useRouter } from 'next/navigation';
+import { MESSAGES } from '@/constants/messages';
+
+const TEMP_PASSWORD = '1234';
 
 export default function AdminButton() {
   const router = useRouter();
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
   const [open, setOpen] = useState(false);
+
+  const validatePassword = (password: string) => {
+    return password == TEMP_PASSWORD;
+  };
 
   const handleClick = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const handleSubmit = () => {
+    if (validatePassword(password)) router.push('/menu');
+    else {
+      setErrorMessage(MESSAGES.invalidPassword);
+      setPassword('');
+    }
+  };
 
   return (
     <>
@@ -28,17 +44,22 @@ export default function AdminButton() {
         <BaseModal onClose={handleClose}>
           <BaseModal.title>관리자 모드 전환</BaseModal.title>
           <BaseModal.content>
-            <TextInput type="password" placeholder="비밀번호" />
+            <TextInput
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                setErrorMessage(undefined);
+              }}
+              type="password"
+              placeholder="비밀번호"
+              errorMessage={errorMessage}
+            />
           </BaseModal.content>
           <BaseModal.actions>
             <BaseButton onClick={handleClose} variant="cancel">
               취소
             </BaseButton>
-            <BaseButton
-              onClick={() => {
-                router.push('/');
-              }}
-            >
+            <BaseButton disabled={password == ''} onClick={handleSubmit}>
               입력하기
             </BaseButton>
           </BaseModal.actions>
--- apps/fe/src/constants/messages.ts
+++ apps/fe/src/constants/messages.ts
@@ -1,6 +1,7 @@
 export const MESSAGES = {
   emailRequired: '이메일을 입력해 주세요.',
   passwordRequired: '비밀번호를 입력해 주세요.',
+  invalidPassword: '비밀번호가 틀렸습니다.',
   invalidCredentials: '이메일 또는 비밀번호가 일치하지 않습니다.',
   featureComingSoon: '해당 기능은 추후 업데이트될 예정입니다.',
   unexpectedError: '알 수 없는 오류가 발생했습니다.',
