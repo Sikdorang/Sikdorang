f478422a127c8efa5d9e76677e757882d1291308
shl0501 <114389927+shl0501@users.noreply.github.com>
2025-05-04 03:16:13 +0900

    fix: get description (#77)
    Co-authored-by: shl0501 <shl050105010501@gmail.com>

--- apps/be/internal/menu/dto/get_description_response.dto.go
+++ apps/be/internal/menu/dto/get_description_response.dto.go
@@ -1,8 +1,19 @@
 package dto
 
 type GetDescriptionResponseDTO struct {
-	Preview string   `json:"preview"`
-	Details string   `json:"details"`
-	Tags    []string `json:"tags"`
-	Images  []string `json:"images"`
+    Preview string           `json:"preview"`
+    Details string           `json:"details"`
+    Tags    []TagsDTO     `json:"tags"`  
+    Images  []ImagesDTO   `json:"images"`
+}
+
+type ImagesDTO struct {
+	ID    uint   `json:"id"`
+	URL   string `json:"url"`
+	Order string    `json:"order"`
+}
+
+type TagsDTO struct {
+	ID  uint   `json:"id"`
+	Tag string `json:"tag"`
 }
