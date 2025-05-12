aa018002240841c3dbf14f4be7255da45817a124
shl0501 <114389927+shl0501@users.noreply.github.com>
2025-05-13 04:50:50 +0900

    fix: create category body (#89)
    Co-authored-by: shl0501 <shl050105010501@gmail.com>

--- apps/be/internal/category/dto/create_category_request.dto.go
+++ apps/be/internal/category/dto/create_category_request.dto.go
@@ -1,5 +1,7 @@
 package dto
 
-type UpdateCategoryRequestDTO struct {
+type CreateCategoryRequestDTO struct {
 	Category string `json:"category" validate:"required" example:"증류주"`
+	Order    string `json:"order" validate:"required" example:"111"`
 }
+
