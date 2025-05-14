972ba1654d22538b55b044f853f03f97e51b2fb6
shl0501 <shl050105010501@gmail.com>
2025-05-14 13:14:11 +0900

    fix: fix menu_mapper and category dto

--- apps/be/internal/category/dto/get_category_response.dto.go
+++ apps/be/internal/category/dto/get_category_response.dto.go
@@ -3,4 +3,5 @@ package dto
 type GetCategoryResponseDTO struct {
 	ID       uint   `json:"id" example:"1"`
 	Category string `json:"category" example:"음료"`
+	Order	 string `json:"order" example:"1"`
 }
