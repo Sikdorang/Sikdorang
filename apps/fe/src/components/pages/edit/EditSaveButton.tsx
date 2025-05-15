ab9147c18c9ecef400de133e054619a802a98db2
Yujin <yujinyll9148@gmail.com>
2025-05-15 13:09:41 +0900

    Fix(fe)/chore (#102)
    * fix: change favicon
    
    * fix: change pwa icon and color
    
    * fix: change menu modal details to be multi lines
    
    * fix: fix font not applied in pwa
    
    * fix: disable edit button before sorting

--- apps/fe/src/components/pages/edit/EditSaveButton.tsx
+++ apps/fe/src/components/pages/edit/EditSaveButton.tsx
@@ -1,12 +1,15 @@
-interface EditSaveButtonProps {
+import { ButtonHTMLAttributes } from 'react';
+
+interface EditSaveButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   onClick: () => void;
 }
 
-export default function EditSaveButton({ onClick }: EditSaveButtonProps) {
+export default function EditSaveButton({ onClick, ...rest }: EditSaveButtonProps) {
   return (
     <button
-      className="text-sm font-bold text-white fixed bottom-5 right-5 bg-blue-500 p-1 w-16 h-16 rounded-full shadow-2xl"
+      className="disabled:bg-gray-300 disabled:text-gray-100 text-sm font-bold text-white fixed bottom-7 right-7 bg-blue-500 p-1 w-20 h-20 rounded-full shadow-2xl"
       onClick={onClick}
+      {...rest}
     >
       저장
     </button>
