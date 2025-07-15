3a812be0073075edd903cb6c7e0affcaa638be20
shl0501 <shl050105010501@gmail.com>
2025-07-15 13:00:04 +0900

    feat: 가게 정보 불러오기

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { PrismaClient } from '@prisma/client';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtService } from './jwt.service';
+
 @Module({
   imports: [PassportModule],
   controllers: [AuthController],
--- apps/backend/src/store/store.controller.ts
+++ apps/backend/src/store/store.controller.ts
@@ -1,30 +1,26 @@
-import {
-  Body,
-  Controller,
-  Patch,
-  Req,
-  UnauthorizedException,
-  UseGuards,
-} from '@nestjs/common';
-import { Request } from 'express';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 
+import { UserId } from '../auth/decorators/user-id.decorator';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 import { CreateStoreDto } from './dto/create-store.dto';
 import { StoreService } from './store.service';
 import { CreateStoreSwagger } from './swagger/create-store.swagger';
+import { GetStoreSwagger } from './swagger/get-store.swagger';
 
 @Controller('store')
+@UseGuards(JwtAuthGuard)
 export class StoreController {
   constructor(private readonly storeService: StoreService) {}
+
   @Patch()
-  @UseGuards(JwtAuthGuard)
   @CreateStoreSwagger()
-  async create(@Body() dto: CreateStoreDto, @Req() req: Request) {
-    const userId = Number(req.user?.['userId']);
-    if (isNaN(userId)) {
-      throw new UnauthorizedException('userId not found');
-    }
+  async createStore(@Body() dto: CreateStoreDto, @UserId() userId: number) {
     return this.storeService.create(dto, userId);
+  }
+  @Get()
+  @GetStoreSwagger()
+  async getStore(@UserId() userId: number) {
+    return this.storeService.getStore(userId);
   }
 }
