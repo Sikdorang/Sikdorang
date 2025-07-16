fcabb5314b1a4db2786a080027db87a8043b9e3c
Yujin Lee <yujinyll@naver.com>
2025-07-16 15:29:35 +0900

    fix: 삼항 연산자로 변경

--- apps/frontend-menu-viewer/src/main.tsx
+++ apps/frontend-menu-viewer/src/main.tsx
@@ -1,11 +1,11 @@
+import App from './App.tsx';
+import './index.css';
 import { enableMapSet } from 'immer';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
 
 async function enableMocking() {
-  if (import.meta.env.VITE_MOCKING == 'true') {
+  if (import.meta.env.VITE_MOCKING === 'true') {
     const { worker } = await import('./mocks/browser');
     await worker.start({
       onUnhandledRequest: 'warn',
