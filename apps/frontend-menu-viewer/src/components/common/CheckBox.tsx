1efb349fd3ec77b8a94801a3607c264970489542
Yujin Lee <yujinyll@naver.com>
2025-07-16 10:05:20 +0900

    refactor: 체크박스랑 수량 체크 컴포넌트 분리

---
+++ apps/frontend-menu-viewer/src/components/common/CheckBox.tsx
@@ -0,0 +1,19 @@
+import CheckSvg from '@/assets/icons/ic_check.svg?react';
+
+interface Props {
+  checked: boolean;
+  onClick?: () => void;
+}
+
+export default function CheckBox({ checked, onClick }: Props) {
+  return (
+    <div
+      onClick={onClick}
+      className={`flex h-6 w-6 items-center justify-center rounded border transition-colors duration-200 ${
+        checked ? 'bg-main-500 border-main-500' : 'border-gray-200'
+      }`}
+    >
+      {checked && <CheckSvg width={20} height={20} />}
+    </div>
+  );
+}
---
+++ apps/frontend-menu-viewer/src/components/common/QuantityCounter.tsx
@@ -0,0 +1,42 @@
+interface QuantityCounterProps {
+  value: number;
+  onChange: (value: number) => void;
+  min?: number;
+  max?: number;
+}
+
+export default function QuantityCounter({
+  value,
+  onChange,
+  min = 1,
+  max,
+}: QuantityCounterProps) {
+  const handleDecrease = () => {
+    if (value > min) onChange(value - 1);
+  };
+  const handleIncrease = () => {
+    if (max === undefined || value < max) onChange(value + 1);
+  };
+
+  return (
+    <div className="self-start flex gap-1 rounded-md bg-gray-200 p-0.5">
+      <button
+        onClick={handleDecrease}
+        className={`transition-colors duration-300 ${value === min ? 'text-gray-400' : 'text-gray-600'} text-mb-1 flex aspect-square h-6 w-6 flex-col items-center justify-center`}
+        disabled={value === min}
+      >
+        -
+      </button>
+      <span className="text-mb-1 flex aspect-square h-6 w-6 flex-col items-center justify-center rounded-md bg-white text-gray-800">
+        {value}
+      </span>
+      <button
+        onClick={handleIncrease}
+        className={`text-mb-1 flex aspect-square h-6 w-6 flex-col items-center justify-center ${max !== undefined && value === max ? 'text-gray-400' : 'text-gray-600'}`}
+        disabled={max !== undefined && value === max}
+      >
+        +
+      </button>
+    </div>
+  );
+}
